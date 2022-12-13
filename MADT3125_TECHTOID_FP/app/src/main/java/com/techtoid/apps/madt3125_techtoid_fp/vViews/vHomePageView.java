4ab36497f8a3ea7f7d7f102213ac625bda8a839d
pavitkalra150 <c0868150@mylambton.ca>
2022-12-13 17:23:39 -0500

    Search feature created and changed layout for Homepage View

--- MADT3125_TECHTOID_FP/app/src/main/java/com/techtoid/apps/madt3125_techtoid_fp/vViews/vHomePageView.java
+++ MADT3125_TECHTOID_FP/app/src/main/java/com/techtoid/apps/madt3125_techtoid_fp/vViews/vHomePageView.java
@@ -16,29 +16,60 @@ import com.techtoid.apps.madt3125_techtoid_fp.mModels.mListItem;
 import com.techtoid.apps.madt3125_techtoid_fp.cControllers.cHomePageController;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class vHomePageView {
     private static Context _context;
     private static FloatingActionButton uiAddNewEmployeeButton;
     private static ListView uiHomePageEmployeeList;
+    private static ArrayList<mListItem> employeesList;
+    public static mHomePageList numbersArrayAdapter;
+
     public static void initUIcomponents(FloatingActionButton floatingActionButton, ListView listView) {
         uiAddNewEmployeeButton = floatingActionButton;
         uiHomePageEmployeeList = listView;
     }
-    public static ListView getUiHomePageEmployeeList() { return uiHomePageEmployeeList; }
-    public static void setActivityContext(Context context) { _context = context; }
+
+    public static ListView getUiHomePageEmployeeList() {
+        return uiHomePageEmployeeList;
+    }
+
+    public static void setActivityContext(Context context) {
+        _context = context;
+    }
+
     public static void PageLoaded() {
+        employeesList = new ArrayList<>();
+        employeesList.addAll(cHomePageController.getEmployees());
         // the context and arrayList created above
-        mHomePageList numbersArrayAdapter = new mHomePageList(_context, cHomePageController.getEmployees());
+        numbersArrayAdapter = new mHomePageList(_context, employeesList);
 
         // set the numbersViewAdapter for ListView
         uiHomePageEmployeeList.setAdapter(numbersArrayAdapter);
-        if(dDatabase.iIntent._canShow) {
-            String message = (dDatabase.iIntent._success)?"Registration is Succesfull":"Failed Registration";
-            Toast.makeText(_context,message,Toast.LENGTH_LONG).show();
+        if (dDatabase.iIntent._canShow) {
+            String message = (dDatabase.iIntent._success) ? "Registration is Succesfull" : "Failed Registration";
+            Toast.makeText(_context, message, Toast.LENGTH_LONG).show();
             dDatabase.iIntent._canShow = false;
         }
+    }
 
+    public static void filterData(String text) {
+        ArrayList<mListItem> dbEmployeesList =cHomePageController.getEmployees();
+        if (text.isEmpty()) {
+            employeesList.clear();
+            employeesList.addAll(dbEmployeesList);
+            numbersArrayAdapter.notifyDataSetChanged();
+            return;
+        }
+        ArrayList<mListItem> filteredEmployeesList = new ArrayList<>();
+        for (int i = 0; i < dbEmployeesList.size(); i++) {
+            mListItem item = dbEmployeesList.get(i);
+            if (item.getmName().toLowerCase().contains(text) || item.getmRole().toLowerCase().contains(text))
+                filteredEmployeesList.add(item);
+        }
+        employeesList.clear();
+        employeesList.addAll(filteredEmployeesList);
+        numbersArrayAdapter.notifyDataSetChanged();
     }
 
 }
